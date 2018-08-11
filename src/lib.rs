cfd0454e0f14fb46cea05a4a0649b5f4519f0e9d
JolliestJames <martinja@oregonstate.edu>
2018-08-11 11:11:08 -0700

    Fix implementation of is_leap_year

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub fn is_leap_year(year: i32) -> bool {
-    if (year % 400) == 0 {
+    if year%400 == 0 {
         true
-    } else if (year % 4) == 0 {
+    } else if year%100 == 0 {
+        false
+    } else if year%4 == 0 {
         true
     } else {
         false
